46a003440cf3f4c4cf9df2c63474406a192ab690
9113359160 <35725839+9113359160@users.noreply.github.com>
2019-08-27 12:36:42 +0530

    Add files via upload

---
+++ lab3.py
@@ -0,0 +1,11 @@
+
+Tuple1 = (0,1,4,6,8,10)
+Tuple2 = ('aman' 'is' 'a' 'good' 'boy')
+Tuple3 = Tuple1 + Tuple2
+print("Tuple 1: ")
+print(Tuple 1)
+print("\nTuples2: ")
+print(Tuple2)
+print("\nTuples after concatenation")
+print(Tuple3)
+
